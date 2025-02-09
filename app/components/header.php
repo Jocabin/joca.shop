e433e3af1067ccb8474fd231895b63d114f449b6
Jocabin <kobyfar@hotmail.com>
2025-02-09 13:29:34 +0100

    Finish the header style and the home/products page

--- app/components/header.php
+++ app/components/header.php
@@ -1,4 +1,4 @@
-<header class="w-full flex flex-row justify-between items-center gap-4 p-4">
+<header class="w-full flex flex-row justify-between items-center gap-4 p-8 border-b border-gray-200 sticky top-0 bg-white">
         <nav class="flex flex-row justify-between items-center gap-4">
                 <a class="uppercase underline hover:no-underline" href="/">Products</a>
                 <a class="uppercase underline hover:no-underline" href="/about">About</a>
--- app/views/home.php
+++ app/views/home.php
@@ -1,16 +1,26 @@
 <?php
-$products = fetch_json("https://api.escuelajs.co/api/v1/products?limit=10&offset=0");
+$products = fetch_json("https://api.escuelajs.co/api/v1/products?limit=28&offset=0");
 ?>
 
 <section>
-        <ul class="flex flex-row gap-4 flex-wrap p-8">
+        <ul class="products_grid p-8 w-full gap-16">
                 <?php foreach ($products as $product): ?>
-                        <li class="flex flex-col gap-4">
-                                <a href="/products/<?= $product['id'] ?>">
-                                        <img src="<?= $product["images"][0] ?>" alt="<?= $product["description"] ?>" width="100" height="100">
-                                        <p><?= $product["title"]; ?></p>
+                        <li>
+                                <a
+                                        href="/products/<?= $product['id'] ?>"
+                                        class="max-w-[200px] h-full flex justify-start items-center flex-col border border-gray-200 hover:border-gray-500 rounded">
+                                        <img src="<?= $product["images"][0] ?>" alt="<?= $product["description"] ?>" width="200" height="200">
+                                        <p class="text-center p-4"><?= $product["title"]; ?></p>
                                 </a>
                         </li>
                 <?php endforeach; ?>
         </ul>
 </section>
+
+<style>
+        .products_grid {
+                display: grid;
+                grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+                justify-items: center;
+        }
+</style>

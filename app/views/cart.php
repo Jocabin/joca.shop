11b005e081a3b02ac34f681beae0e6e640499eec
Jocabin <kobyfar@hotmail.com>
2025-02-12 17:52:05 +0100

    Add html and css for cart page

--- app/views/cart.php
+++ app/views/cart.php
@@ -1,6 +1,17 @@
 <?php
-if (empty($_SESSION['cart'])) {
-        echo 'panier vide';
-} else {
-        var_dump($_SESSION['cart']);
-}
+$is_cart_empty = empty($_SESSION['cart']);
+$cart = $_SESSION['cart'];
+?>
+
+<div>
+        <ul class="flex flex-row justify-center items-center gap-16 flex-wrap">
+                <?php foreach ($cart as $item): ?>
+                        <li>
+                                <a href="/products/<?= $item['id'] ?>" class="link max-w-[400px] max-h-[400px] flex flex-col justify-center items-center gap-4">
+                                        <img src="<?= $item['image'] ?>" alt="<?= $item['description'] ?>" width="200" height="200" class="object-contain p-4 w-[200px] h-[200px]">
+                                        <span class="text-center"><?= $item['name'] ?></span>
+                                </a>
+                        </li>
+                <?php endforeach; ?>
+        </ul>
+</div>

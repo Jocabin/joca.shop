4704516a996d2d249602e09899b23feb7f979f45
Jocabin <kobyfar@hotmail.com>
2025-02-12 19:00:06 +0100

    Fix cart display when there is no items

--- app/views/cart.php
+++ app/views/cart.php
@@ -8,20 +8,25 @@ foreach ($cart as $item) {
 }
 ?>
 
-<div>
-        <ul class="flex flex-row justify-center items-center gap-16 flex-wrap">
-                <?php foreach ($cart as $item): ?>
-                        <li>
-                                <a href="/products/<?= $item['id'] ?>" class="link max-w-[400px] max-h-[400px] flex flex-col justify-center items-center gap-4">
-                                        <img src="<?= $item['image'] ?>" alt="<?= $item['description'] ?>" width="200" height="200" class="object-contain p-4 w-[200px] h-[200px]">
-                                        <span class="text-center"><?= $item['name'] ?></span>
-                                </a>
-                        </li>
-                <?php endforeach; ?>
-        </ul>
+<div class="w-full">
+        <h1 class="text-center font-bold text-2xl my-8">Your cart.</h1>
+        <?php if (empty($cart)): ?>
+                <p class="text-center">Your cart is empty. See our products <a href="/" class="link">here</a>.</p>
+        <?php else: ?>
+                <ul class="flex flex-row justify-center items-center gap-16 flex-wrap px-8">
+                        <?php foreach ($cart as $item): ?>
+                                <li>
+                                        <a href="/products/<?= $item['id'] ?>" class="link max-w-[400px] max-h-[400px] flex flex-col justify-center items-center gap-4">
+                                                <img src="<?= $item['image'] ?>" alt="<?= $item['description'] ?>" width="200" height="200" class="object-contain p-4 w-[200px] h-[200px]">
+                                                <span class="text-center"><?= $item['name'] ?></span>
+                                        </a>
+                                </li>
+                        <?php endforeach; ?>
+                </ul>
 
-        <div class="w-full sticky bottom-0 bg-gray-100 p-8 my-8 flex flex-col justify-center items-center gap-4">
-                <p>Total price: <span class="font-bold"><?= $cart_total ?>€</span></p>
-                <a class="button" href="/checkout">Proceed to checkout</a>
-        </div>
+                <div class="w-full sticky bottom-0 bg-gray-100 p-8 my-8 flex flex-col justify-center items-center gap-4">
+                        <p>Total price: <span class="font-bold"><?= $cart_total ?>€</span></p>
+                        <a class="button" href="/checkout">Proceed to checkout</a>
+                </div>
+        <?php endif; ?>
 </div>

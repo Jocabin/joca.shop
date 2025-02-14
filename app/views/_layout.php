4d009bd03dd66f582d31d0d2f1f3c44177b26753
Jocabin <kobyfar@hotmail.com>
2025-02-14 09:14:21 +0100

    Remove comments

--- app/views/_layout.php
+++ app/views/_layout.php
@@ -1,5 +1,3 @@
-<!-- todo: optimiser selon la vidéo de wesbos -->
-<!-- todo: multilang website -->
 <?php global $request_context; ?>
 
 <!DOCTYPE html>
@@ -12,7 +10,6 @@
         <meta name="description" content="A simplest but functionnal e-commerce website." />
         <link rel="icon" type="image/svg+xml" href="/public/favicon.svg" />
         <link rel="preload" href="/public/roboto.woff2" as="font" type="font/woff2" crossorigin />
-        <!-- todo: use tailwind cli for production -->
         <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
         <link rel="stylesheet" href="/public/styles.css">
         <style type="text/tailwindcss">
@@ -21,6 +18,9 @@
                 }
                 .link {
                         @apply underline hover:no-underline uppercase cursor-pointer;
+                }
+                .h1 {
+                        @apply font-bold text-2xl;
                 }
         </style>
         <script src="https://unpkg.com/htmx.org@2.0.4" integrity="sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+" crossorigin="anonymous"></script>
